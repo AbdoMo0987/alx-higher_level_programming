041d0d5286f4421ffdb14156966ac4511577c53b
AbdoMo0987 <madridiahmed481@gmail.com>
2024-01-05 04:35:12 -0800

    a

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """ Add all arguments."""
-    import sys
+    """Print all hidden directories"""
+    import hidden_4
 
-    result = 0
-
-    for i in range(len(sys.argv) - 1):
-        result += (int(sys.argv[i + 1]))
-    print("{:d}".format(result))
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
